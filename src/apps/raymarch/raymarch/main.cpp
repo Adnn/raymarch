2a7ba0779f31840a8783fc5b54b7f0ac997cdbdc
Adrien <Adnn@users.noreply.github.com>
2024-01-26 15:00:43 +0100

    Implement normals and a naive illumination model.

--- src/apps/raymarch/raymarch/main.cpp
+++ src/apps/raymarch/raymarch/main.cpp
@@ -41,6 +41,25 @@ float distanceSphere(vec3 p, vec3 c, float r)
     return length(p - c) - r;
 }
 
+float eval(vec3 p)
+{
+    const vec3 sphere = vec3(0., 0., -3.);
+    const float r = 1;
+
+    return distanceSphere(p, sphere, r);
+}
+
+vec3 getNormal(vec3 p)
+{
+    const vec3 eps = vec3(0.001, 0., 0.);
+
+    return normalize(vec3(
+        eval(p + eps.xyz) - eval(p - eps.xyz),
+        eval(p + eps.yxz) - eval(p - eps.yxz),
+        eval(p + eps.yzx) - eval(p - eps.yzx)
+    ));
+}
+
 out vec4 fragColor;
 
 uniform ivec2 uViewportSize;
@@ -52,9 +71,6 @@ const int MAX_STEPS = 20;
 void main()
 {
     const vec3 camera = vec3(0., 0., 5.);
-
-    const vec3 sphere = vec3(0., 0., -3.);
-    const float r = 1;
 
     vec3 fragmentGridPos = vec3(
         (gl_FragCoord.xy / uViewportSize) * 2 - 1,
@@ -68,11 +84,15 @@ void main()
     vec3 currentPos = fragmentGridPos;
     for (int i = 0; i < MAX_STEPS; ++i)
     {
-        float closest = distanceSphere(currentPos, sphere, r);
+        float closest = eval(currentPos);
 
         if(closest < MAX_HIT_DISTANCE)
         {
-            fragColor = vec4(1., 1., 1., 1.);
+            vec3 albedo = vec3(1., 1., 1.);
+
+            float d = max(0., dot(getNormal(currentPos), vec3(-1., 0., 0.5)));
+
+            fragColor = vec4((d * albedo * 0.7 + albedo * 0.3), 1.0);
             return;
         }
 
